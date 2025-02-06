07fab15140a9abebce06c9caf498902621a79b9e
Talipov S.N. <proffix4@gmail.com>
2025-02-06 15:18:11 +0500

    update_20250206151811

---
+++ git_push.bat
@@ -0,0 +1,17 @@
+﻿@echo off
+cd /d "%~dp0"
+
+echo Добавление всех изменений...
+git add .
+
+for /f "tokens=2 delims==." %%I in ('wmic os get localdatetime /value') do set datetime=%%I
+set commit_msg=update_%datetime%
+
+echo Создание коммита...
+git commit -m "%commit_msg%"
+
+echo Отправка на GitHub...
+git push origin main
+
+echo Готово!
+pause
